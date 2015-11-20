9168a59204eeeee1616f66b2056df2aea63b9099
ellekim89 <ellekim89@gmail.com>
2015-11-19 20:08:40 -0800

    html, css, js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  var scroll = function(where){
+    $('html, body').animate({
+      scrollTop: where.offset().top
+    }, 500);
+  };
+
+   $('#Zero').on('click', function(){
+        scroll($("#main"));
+    });
+
+   $('.reason').on('click', function(){
+        scroll($("#reason" + this.id));
+    });
+  });
